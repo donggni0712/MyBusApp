ed7f391e795d41d47f7ef3f186ea0eb7995ce19c
dongjae <dhehdwo7@ajou.ac.kr>
2020-07-31 21:24:40 +0900

    Add BusArrivalListApiClient in /api.

---
+++ myBusApp/src/main/java/MyBusApp/api/BusArrivalListApiClient.java
@@ -0,0 +1,23 @@
+package MyBusApp.api;
+
+import MyBusApp.domain.BusArrivalItem;
+import MyBusApp.domain.BusArrivalList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class BusArrivalListApiClient {
+    @Autowired
+    RestTemplate restTemplate = new RestTemplate();
+    @Autowired
+    ApikeyManager apikeyManager = new ApikeyManager();
+
+    private String BUSARRIVALITEM_REQUEST_URI = "https://www.data.go.kr/iim/api/getBusArrivalList?serviceKey={apikey}&stationId={stationId}";
+    public BusArrivalList getBusArrivalList(int stationId){
+        String apikey = apikeyManager.getapikey();
+        BusArrivalList busArrivalList = restTemplate.getForObject(BUSARRIVALITEM_REQUEST_URI, BusArrivalList.class,apikey,stationId);
+        return busArrivalList;
+    }
+
+}
